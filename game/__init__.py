b4859be0e61bc938072e74e0e6902c738ac5d267
DOMOG001 <guillaume.dormoy@disney.com>
2018-07-04 14:17:05 +0200

    add sprit, and rex movement

--- game/__init__.py
+++ game/__init__.py
@@ -12,27 +12,26 @@ white = [247, 247, 247]
 screen.fill(white)
 surface = pygame.display.get_surface()
 rex = pygame.image.load("images/t-rex.png").convert_alpha()
-rexPosX =  100
-rexPosY = surface.get_height() - 45
-screen.blit(rex ,(rexPosX,rexPosY))
+position_rex = rex.get_rect()
+position_rex = position_rex.move(100,surface.get_height() - 45)
+screen.blit(rex ,position_rex)
 
 pygame.display.flip()
 
 continuer = 1
 
 while continuer:
-
-
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = 0
-        if event.type == KEYDOWN and event.key == K_UP:
-            for i in range (0, 150):
-                if i < 75:
-                    rexPosY -= 1
-                    sleep(0.003)
-                else:
-                    rexPosY += 1
-                    sleep(0.003)
-                screen.blit(rex, (rexPosX, rexPosY))
-                pygame.display.flip()
+        if event.type == KEYDOWN:
+            if event.key == K_UP or event.key == K_SPACE:
+                for i in range (0, 150):
+                    if i < 75:
+                        position_rex = position_rex.move(0, -1)
+                        sleep(0.003)
+                    else:
+                        position_rex = position_rex.move(0, 1)
+                        sleep(0.003)
+                    screen.blit(rex, position_rex)
+                    pygame.display.flip()
