d2c45c47af823fc93f97f4791557766fa9c3849c
DOMOG001 <guillaume.dormoy@disney.com>
2018-07-04 12:26:26 +0200

    add sleep during rex jump

--- game/__init__.py
+++ game/__init__.py
@@ -1,11 +1,14 @@
 import pygame
+import time
+
 from pygame.locals import *
+from time import sleep
 
 pygame.init()
 
 
 screen = pygame.display.set_mode((640, 480))
-white = [255, 255, 255]
+white = [247, 247, 247]
 screen.fill(white)
 surface = pygame.display.get_surface()
 rex = pygame.image.load("images/t-rex.png").convert_alpha()
@@ -18,14 +21,18 @@ pygame.display.flip()
 continuer = 1
 
 while continuer:
+
+
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = 0
         if event.type == KEYDOWN and event.key == K_UP:
-            for i in range (0, 200):
-                if i < 100:
+            for i in range (0, 150):
+                if i < 75:
                     rexPosY -= 1
+                    sleep(0.003)
                 else:
                     rexPosY += 1
+                    sleep(0.003)
                 screen.blit(rex, (rexPosX, rexPosY))
                 pygame.display.flip()
