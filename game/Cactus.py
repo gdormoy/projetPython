25804c4dd43b7ec839f0b1cbd8c410f1101b9f82
dormoy <dormoy.guillaume@gmail.com>
2018-07-21 16:17:34 +0200

    add cactus

--- game/Cactus.py
+++ game/Cactus.py
@@ -23,28 +23,29 @@ class Cactus(Thread):
 
     cactu = [0] * 64
     surface = None
+    play = True
+
     position_cactus = None
-    white = (255, 255, 255)
 
     def __init__(self, screen, surface):
         Thread.__init__(self)
         self.screen = screen
         self.surface = surface
-        self.cactu[0] = pygame.image.load(self.blancPng)
+        self.cactu[0] = pygame.image.load(self.blancPng).convert_alpha()
         self.cactu[0].set_colorkey((255, 255, 255))
         self.position_cactus = self.cactu[0].get_rect()
         self.position_cactus = self.position_cactus.move(0, self.surface.get_height() - self.cactu[0].get_rect().bottom)
         for i in range(len(self.cactu)):
-            self.cactu[i] = pygame.image.load(self.blancPng)
+            self.cactu[i] = pygame.image.load(self.blancPng).convert_alpha()
             self.screen.blit(self.cactu[i], self.position_cactus)
             self.cactu[i].set_colorkey((255, 255, 255))
             pygame.display.flip()
             self.position_cactus = self.position_cactus.move(16,0)
-        pygame.display.flip()
+        pygame.display.update()
 
 
     def slide_cactus(self):
-        x = random.randint(0, 100)
+        x = random.randint(0, 500)
         print(x)
         self.position_cactus = self.cactu[0].get_rect()
         self.position_cactus = self.position_cactus.move(0, self.surface.get_height() - self.cactu[0].get_rect().bottom)
@@ -52,11 +53,12 @@ class Cactus(Thread):
             self.cactu[i] = self.cactu[i + 1]
             self.cactu[i].set_colorkey((255, 255, 255))
             self.screen.blit(self.cactu[i], self.position_cactus)
-            pygame.display.flip()
-            self.position_cactus = self.position_cactus.move(16, self.surface.get_height() - self.cactu[i].get_rect().bottom)
-        if x < 80:
+            pygame.display.update()
+            self.position_cactus = self.position_cactus.move(16, 0)
+        if x > 490:
             self.position_cactus = self.position_cactus.move(16, self.surface.get_height() - self.cactu[len(self.cactu) - 1].get_rect().bottom)
-            self.cactu[len(self.cactu) - 1] = pygame.image.load(random.choice(self.cactus)).convert_alpha()
+            self.cactu[len(self.cactu) - 1] = pygame.image.load(random.choice(self.cactus))
+            self.cactu[len(self.cactu) - 1].set_colorkey((255, 255, 255))
         else:
             self.cactu[len(self.cactu) - 1] = pygame.image.load(self.blancPng)
             self.cactu[len(self.cactu) - 1].set_colorkey((255, 255, 255))
@@ -65,6 +67,5 @@ class Cactus(Thread):
         # pygame.display.flip()
 
     def run(self):
-        play = True
-        while play:
+        while self.play:
             self.slide_cactus()
--- game/__init__.py
+++ game/__init__.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 from game.Afficheur import Afficheur
 from game.Ground import Ground
 from game.Rex import Rex
+from game.Cactus import Cactus
 
 pygame.init()
 
@@ -14,13 +15,12 @@ surface = pygame.display.get_surface()
 clock = pygame.time.Clock()
 #
 rex = pygame.image.load("images/rex/t-rex-start.png").convert_alpha()
-
+# pygame.Surface.set_colorkey(255,255,255)
 ptera = pygame.image.load("images/ptera/ptera1.png").convert_alpha()
 position_rex = rex.get_rect()
 position_rex = position_rex.move(100,surface.get_height() - 50)
 screen.blit(rex ,position_rex)
 
-pygame.display.flip()
 pygame.key.set_repeat(400, 30)
 continuer = 1
 
@@ -29,13 +29,14 @@ continuer = 1
 def jump(position_rex):
     rex = pygame.image.load("images/rex/t-rex1.png").convert_alpha()
     frame = pygame.Surface((position_rex.width, position_rex.height))
-    for i in range(0, 180):
+    for i in range(0, 280):
         frame.fill(white)
         screen.blit(frame, position_rex)
-        if i < 90:
+        if i < 100:
             position_rex = position_rex.move(0, -1)
-        else:
+        elif i > 180 :
             position_rex = position_rex.move(0, 1)
+        rex.set_colorkey((255, 255, 255))
         rex.set_colorkey((255, 255, 255))
         screen.blit(rex, position_rex)
         clock.tick(500)
@@ -80,11 +81,14 @@ def start():
                 if event.key == K_UP or event.key == K_SPACE:
                     play = 0
 
+cactus = Cactus(screen,surface)
 ground = Ground(screen,surface)
 afficheur = Afficheur(screen)
 # rex = Rex(screen, surface)
 start()
 # rex.jump()
+cactus.start()
+pygame.display.flip()
 jump(position_rex)
 afficheur.start()
 ground.start()
